2dd5a96431986544f14a01fee05726393cf3fd35
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-09-07 09:13:56 +0400

    Create app-set.tf

---
+++ terraform/app-set.tf
@@ -0,0 +1,39 @@
+resource "kubectl_manifest" "app_set" {
+  yaml_body = <<YAML
+
+apiVersion: argoproj.io/v1alpha1
+kind: ApplicationSet
+metadata:
+  name: git-generator
+  namespace: argocd
+spec:
+  generators:
+  - git:
+      repoURL: https://github.com/tereza-arta/argo-apps.git
+      revision: main
+      directories:
+      - path: gen/res/apps/*
+  template:
+    metadata:
+      name: '{{path.basename}}-application'
+    spec:
+      project: default
+      source:
+        repoURL: https://github.com/tereza-arta/argo-apps.git
+        targetRevision: main
+        path: '{{path}}'
+      destination:
+        server: https://kubernetes.default.svc
+        namespace: '{{path.basename}}-ns'
+      syncPolicy:
+        automated:
+          prune: true
+          selfHeal: true
+          allowEmpty: false
+        syncOptions:
+          - CreateNamespace=true
+
+YAML
+
+  depends_on = [null_resource.install_app_set_crd]
+}
