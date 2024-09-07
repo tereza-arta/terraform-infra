02858f40c55a88762ae792c07d62468fde6bf07a
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-09-07 09:10:49 +0400

    Create ip-addr-pool.tf

---
+++ terraform/ip-addr-pool.tf
@@ -0,0 +1,16 @@
+resource "kubectl_manifest" "addr_pool" {
+  yaml_body = <<YAML
+
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: addr-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - 192.168.49.220-192.168.49.230
+
+YAML
+
+  depends_on = [helm_release.metallb]
+}
